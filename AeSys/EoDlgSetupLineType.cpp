f65ea4246be0bfa0660707231eddade6b4252e5e
Terry A. Smith <t.smith@swbell.net>
2019-03-13 16:33:43 -0500

    Update EoDlgSetupLineType.cpp

--- AeSys/EoDlgSetupLineType.cpp
+++ AeSys/EoDlgSetupLineType.cpp
@@ -19,7 +19,7 @@ void EoDlgSetupLinetype::DoDataExchange(CDataExchange* pDX) {
 	DDX_Control(pDX, IDC_LINETYPES_LIST_CONTROL, m_LinetypesListControl);
 }
 EoDlgSetupLinetype::EoDlgSetupLinetype(CWnd* parent /*=NULL*/) :
-	CDialog(EoDlgSetupLinetype::IDD, parent) {
+CDialog(EoDlgSetupLinetype::IDD, parent) {
 }
 EoDlgSetupLinetype::EoDlgSetupLinetype(OdDbLinetypeTablePtr linetypeTable, CWnd* parent /*=NULL*/)
 	: CDialog(EoDlgSetupLinetype::IDD, parent), m_LinetypeTable(linetypeTable) {
@@ -36,65 +36,67 @@ void EoDlgSetupLinetype::OnDrawItem(int controlIdentifier, LPDRAWITEMSTRUCT draw
 	if (controlIdentifier == IDC_LINETYPES_LIST_CONTROL) {
 		switch (drawItemStruct->itemAction) {
 		case ODA_DRAWENTIRE: {
-				CRect ItemRectangle(drawItemStruct->rcItem);
-				COLORREF BackgroundColor = ::GetSysColor((drawItemStruct->itemState & ODS_SELECTED) ? COLOR_HIGHLIGHT : COLOR_WINDOW);
+			CRect ItemRectangle(drawItemStruct->rcItem);
+			COLORREF BackgroundColor = ::GetSysColor((drawItemStruct->itemState & ODS_SELECTED) ? COLOR_HIGHLIGHT : COLOR_WINDOW);
 
-				CDC DeviceContext;
-				DeviceContext.Attach(drawItemStruct->hDC);
-				CBrush BackgroundBrush(BackgroundColor);
-				DeviceContext.FillRect(ItemRectangle, &BackgroundBrush);
+			CDC DeviceContext;
+			DeviceContext.Attach(drawItemStruct->hDC);
+			CBrush BackgroundBrush(BackgroundColor);
+			DeviceContext.FillRect(ItemRectangle, &BackgroundBrush);
 
-				if (drawItemStruct->itemState & ODS_FOCUS) {
-					DeviceContext.DrawFocusRect(ItemRectangle);
-				}
-				int Item = drawItemStruct->itemID;
-				if (Item != - 1) {
-					COLORREF rgbText = (drawItemStruct->itemState & ODS_SELECTED) ? ::GetSysColor(COLOR_HIGHLIGHTTEXT) : ::GetSysColor(COLOR_WINDOWTEXT);
-					DeviceContext.SetBkColor(BackgroundColor);
-					DeviceContext.SetTextColor(rgbText);
-
-					OdDbObjectId ItemData = (OdDbStub*)(DWORD)m_LinetypesListControl.GetItemData(Item);
-					OdDbLinetypeTableRecordPtr Linetype = ItemData.safeOpenObject(OdDb::kForRead);
-					
-					CRect SubItemRectangle;
-					m_LinetypesListControl.GetSubItemRect(Item, Name, LVIR_LABEL, SubItemRectangle);
-					OdString Name = Linetype->getName();
-					DeviceContext.ExtTextOutW(SubItemRectangle.left + 6, SubItemRectangle.top + 1, ETO_CLIPPED, &SubItemRectangle, Name, Name.getLength(), NULL);
-
-					m_LinetypesListControl.GetSubItemRect(Item, Appearance, LVIR_LABEL, SubItemRectangle);
-					
-					pstate.SetPen(NULL, &DeviceContext, 0, EoDbLinetypeTable::LegacyLinetypeIndex(Name));
-
-					AeSysView* ActiveView = AeSysView::GetActiveView();
-
-					ActiveView->ViewportPushActive();
-					ActiveView->PushViewTransform();
-
-					ActiveView->SetViewportSize(SubItemRectangle.right + SubItemRectangle.left, SubItemRectangle.bottom + SubItemRectangle.top);
-
-					double FieldWidth = static_cast<double>(SubItemRectangle.right + SubItemRectangle.left) / static_cast<double>(DeviceContext.GetDeviceCaps(LOGPIXELSX));
-					double FieldHeight = static_cast<double>(SubItemRectangle.bottom + SubItemRectangle.top) / static_cast<double>(DeviceContext.GetDeviceCaps(LOGPIXELSY));
-					ActiveView->ModelViewInitialize();
-
-					ActiveView->SetViewWindow(0., 0., FieldWidth, FieldHeight);
-					ActiveView->SetCameraTarget(OdGePoint3d::kOrigin);
-					ActiveView->SetCameraPosition(OdGeVector3d::kZAxis);
-					double FieldWidthMinimum = static_cast<double>(SubItemRectangle.left) / static_cast<double>(DeviceContext.GetDeviceCaps(LOGPIXELSX));
-					double FieldWidthMaximum = static_cast<double>(SubItemRectangle.right) / static_cast<double>(DeviceContext.GetDeviceCaps(LOGPIXELSX));
-
-					EoGeLineSeg3d Line = EoGeLineSeg3d(OdGePoint3d(FieldWidthMinimum, FieldHeight / 2., 0.), OdGePoint3d(FieldWidthMaximum, FieldHeight / 2., 0.));
-					Line.Display(ActiveView, &DeviceContext);
-
-					ActiveView->PopViewTransform();
-					ActiveView->ViewportPopActive();
-
-					m_LinetypesListControl.GetSubItemRect(Item, Description, LVIR_LABEL, SubItemRectangle);
-					CString Description = Linetype->comments();
-					DeviceContext.ExtTextOutW(SubItemRectangle.left + 6, SubItemRectangle.top + 1, ETO_CLIPPED, &SubItemRectangle, Description, Description.GetLength(), NULL);
-				}
-				DeviceContext.Detach();
+			if (drawItemStruct->itemState & ODS_FOCUS) {
+				DeviceContext.DrawFocusRect(ItemRectangle);
 			}
-			break;
+			int Item = drawItemStruct->itemID;
+			if (Item != -1) {
+				COLORREF rgbText = (drawItemStruct->itemState & ODS_SELECTED) ? ::GetSysColor(COLOR_HIGHLIGHTTEXT) : ::GetSysColor(COLOR_WINDOWTEXT);
+				DeviceContext.SetBkColor(BackgroundColor);
+				DeviceContext.SetTextColor(rgbText);
+
+				OdDbObjectId ItemData = (OdDbStub*)(DWORD)m_LinetypesListControl.GetItemData(Item);
+				OdDbLinetypeTableRecordPtr Linetype = ItemData.safeOpenObject(OdDb::kForRead);
+
+				CRect SubItemRectangle;
+				m_LinetypesListControl.GetSubItemRect(Item, Name, LVIR_LABEL, SubItemRectangle);
+				OdString Name = Linetype->getName();
+				DeviceContext.ExtTextOutW(SubItemRectangle.left + 6, SubItemRectangle.top + 1, ETO_CLIPPED, &SubItemRectangle, Name, Name.getLength(), NULL);
+
+				m_LinetypesListControl.GetSubItemRect(Item, Appearance, LVIR_LABEL, SubItemRectangle);
+
+				EoInt16 ColorIndex = pstate.ColorIndex();
+				pstate.SetPen(NULL, &DeviceContext, 0, EoDbLinetypeTable::LegacyLinetypeIndex(Name));
+
+				AeSysView* ActiveView = AeSysView::GetActiveView();
+
+				ActiveView->ViewportPushActive();
+				ActiveView->PushViewTransform();
+
+				ActiveView->SetViewportSize(SubItemRectangle.right + SubItemRectangle.left, SubItemRectangle.bottom + SubItemRectangle.top);
+
+				double FieldWidth = static_cast<double>(SubItemRectangle.right + SubItemRectangle.left) / static_cast<double>(DeviceContext.GetDeviceCaps(LOGPIXELSX));
+				double FieldHeight = static_cast<double>(SubItemRectangle.bottom + SubItemRectangle.top) / static_cast<double>(DeviceContext.GetDeviceCaps(LOGPIXELSY));
+				ActiveView->ModelViewInitialize();
+
+				ActiveView->SetViewWindow(0., 0., FieldWidth, FieldHeight);
+				ActiveView->SetCameraTarget(OdGePoint3d::kOrigin);
+				ActiveView->SetCameraPosition(OdGeVector3d::kZAxis);
+				double FieldWidthMinimum = static_cast<double>(SubItemRectangle.left) / static_cast<double>(DeviceContext.GetDeviceCaps(LOGPIXELSX));
+				double FieldWidthMaximum = static_cast<double>(SubItemRectangle.right) / static_cast<double>(DeviceContext.GetDeviceCaps(LOGPIXELSX));
+
+				EoGeLineSeg3d Line = EoGeLineSeg3d(OdGePoint3d(FieldWidthMinimum, FieldHeight / 2., 0.), OdGePoint3d(FieldWidthMaximum, FieldHeight / 2., 0.));
+				Line.Display(ActiveView, &DeviceContext);
+
+				ActiveView->PopViewTransform();
+				ActiveView->ViewportPopActive();
+
+				m_LinetypesListControl.GetSubItemRect(Item, Description, LVIR_LABEL, SubItemRectangle);
+				CString Description = Linetype->comments();
+				DeviceContext.ExtTextOutW(SubItemRectangle.left + 6, SubItemRectangle.top + 1, ETO_CLIPPED, &SubItemRectangle, Description, Description.GetLength(), NULL);
+				pstate.SetColorIndex(&DeviceContext, ColorIndex);
+			}
+			DeviceContext.Detach();
+		}
+		break;
 
 		case ODA_SELECT:
 			::InvertRect(drawItemStruct->hDC, &(drawItemStruct->rcItem));
@@ -132,6 +134,8 @@ BOOL EoDlgSetupLinetype::OnInitDialog() {
 	return TRUE;
 }
 void EoDlgSetupLinetype::OnOK() {
+	m_Linetype = m_Linetype = m_LinetypeTable->getAt(L"Continuous").safeOpenObject(OdDb::kForRead);
+
 	POSITION Position = m_LinetypesListControl.GetFirstSelectedItemPosition();
 	if (Position != NULL) {
 		int Item = m_LinetypesListControl.GetNextSelectedItem(Position);
